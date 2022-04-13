a808e0d2bb363fe5465fe93940d7e01a51562788
crystalwidjaja <pansypig@gmail.com>
2022-04-12 23:43:11 -0700

    Fix Strings

--- queues/Strings.java
+++ queues/Strings.java
@@ -2,28 +2,28 @@ package queues;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// the snakes class
+// Organizing Strings
 public class Strings {
 
-    public static void main(String[] args)
-    {
-        Queue<String> q = new LinkedList<>();
+	public static void main(String[] args)
+	{
+		Queue<String> q = new LinkedList<>();
 
-        // define the array with an array literal
-        String[] stringArray = new String[]{ "seven", "slimy", "snakes", "sallying", "slowly", "slithered", "southward" };
+		// define array
+    String[] stringArray = new String[]{ "seven", "slimy", "snakes", "sallying", "slowly", "slithered", "southward" }; 
 
-        // push to the queue
-        for (String addWord : stringArray){
-            q.add(addWord);
-            System.out.println("Enqueued data: " + addWord);
-            System.out.println("Words count: " + q.size() + ", data: " + q);
-        }
-
-        // pop from the queue
-        for (int i = 0; i < 7; i++){
-            String removeWord = q.remove();
-            System.out.println("Dequeued data: " + removeWord);
-            System.out.println("Words count: " + q.size() + ", data: " + q);
-        }
+    // push queue
+    for (String addWord : stringArray){
+      q.add(addWord);
+      System.out.println("Enqueued data: " + addWord);
+      System.out.println("Words count: " + q.size() + ", data: " + q);
     }
+
+		// pop queue
+    for (int i = 0; i < 7; i++){
+      String removeWord = q.remove();
+      System.out.println("Dequeued data: " + removeWord);
+      System.out.println("Words count: " + q.size() + ", data: " + q);
+    }
+	}
 }
