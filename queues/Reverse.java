468f28bac811fd4615c63e52fc727ca467982963
crystalwidjaja <pansypig@gmail.com>
2022-04-12 23:42:30 -0700

    Fix reverse.java

--- queues/Reverse.java
+++ queues/Reverse.java
@@ -2,32 +2,32 @@ package queues;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// the Reverse class
+// Reverse class
 public class Reverse {
 
-    public static void main(String[] args)
-    {
-        Queue<Integer> queue1 = new LinkedList<>();
-        Queue<Integer> queue2 = new LinkedList<>();
+	public static void main(String[] args)
+	{
+		Queue<Integer> queue1 = new LinkedList<>();
+    Queue<Integer> queue2 = new LinkedList<>();
 
-        // define the array with an array literal
-        int[] array1 = new int[]{ 1, 2, 3 };
+		// define array
+    int[] array1 = new int[]{ 1, 2, 3 }; 
 
-        // create the two queues
-        for (int addInt : array1){
-            queue1.add(addInt);
-        }
-
-        System.out.println("Here is the starting queue:");
-        System.out.println(queue1);
-
-        // merge into a singular queue
-        for (int i = 0; i < 3; i++){
-            int flipNum = queue1.remove();
-            queue2.add(flipNum);
-        }
-        // print out the merged queue
-        System.out.println("\nHere is the flipped queue:");
-        System.out.println(queue2 + "\n");
+    // create two queues
+    for (int addInt : array1){
+      queue1.add(addInt);
     }
+
+    System.out.println("Here is the starting queue:");
+    System.out.println(queue1);
+
+		// merge queues
+    for (int i = 0; i < 3; i++){
+      int flipNum = queue1.remove();
+      queue2.add(flipNum);
+    }
+    // print merged queue
+    System.out.println("\nHere is the flipped queue:");
+    System.out.println(queue2 + "\n");
+	}
 }
